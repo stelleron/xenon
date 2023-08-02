61ad69b34b57be1e5fe6f4a702600f183e8e757c
stelleron <shreyasdonti223@gmail.com>
2023-08-02 21:38:48 +0530

    Made some minor tweaks for debugging

--- xenon/gfx/sprite.cpp
+++ xenon/gfx/sprite.cpp
@@ -47,7 +47,6 @@ namespace xenon {
         
         glGenTextures(1, &id);
         glBindTexture(GL_TEXTURE_2D, id);
-        glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
         // Default texture params
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);  
@@ -57,6 +56,7 @@ namespace xenon {
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_SWIZZLE_B, GL_BLUE);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_SWIZZLE_A, GL_ALPHA);
         // Load empty texture
+        glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, NULL);
         glGenerateMipmap(GL_TEXTURE_2D);
         glBindTexture(GL_TEXTURE_2D, 0);
--- xenon/gfx/surface.cpp
+++ xenon/gfx/surface.cpp
@@ -34,9 +34,7 @@ namespace xenon {
         this->height = height;
 
         // Now attach a color texture
-        glBindTexture(GL_TEXTURE_2D, 0); 
         color_tex.load(width, height);
-        glBindTexture(GL_TEXTURE_2D, color_tex.id);
         glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, color_tex.id, 0);
 
         // Now attach a depth texture
@@ -46,13 +44,14 @@ namespace xenon {
         glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, depth_tex.id);
         depth_tex.width = width;
         depth_tex.height = height;
-        
+            
         // Unbind framebuffer and texture
         if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
             ERROR("Error: Framebuffer is not complete!");
-            
+                
         glBindFramebuffer(GL_FRAMEBUFFER, 0);
         glBindTexture(GL_TEXTURE_2D, 0); 
+        glBindRenderbuffer(GL_RENDERBUFFER, 0);
     }
 
     bool Surface::is_loaded() {
