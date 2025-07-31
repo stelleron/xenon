23456effa0f8a4b13369ac2df20dba69b935f006
stelleron <shreyasdonti223@gmail.com>
2025-07-31 14:02:15 +0530

    Added look point for which direction the player is facing

--- src/game/player.cpp
+++ src/game/player.cpp
@@ -12,19 +12,46 @@ Player::~Player() {
 }
 
 void Player::init(Context& ctx) {
-    rect = Rectangle(VIRTUAL_WIDTH/2 - 10, VIRTUAL_HEIGHT/2 - 10, 20, 20);
+    rect = Rectangle(
+        (VIRTUAL_WIDTH - PLAYER_SIZE)/2,
+        (VIRTUAL_HEIGHT - PLAYER_SIZE)/2,
+        PLAYER_SIZE,
+        PLAYER_SIZE
+    );
+    look_point = Circle(
+        Vector2{VIRTUAL_WIDTH/2, VIRTUAL_HEIGHT/2},
+        4
+    );
 }
 
 void Player::update(Context& ctx) {
-    if (ctx.keyboard.is_held(xenon::Key::W)) {rect.pos.y -= PLAYER_SPEED * ctx.timer.dt();}
-    if (ctx.keyboard.is_held(xenon::Key::S)) {rect.pos.y += PLAYER_SPEED * ctx.timer.dt();}
+    // Vertical movement
+    if (ctx.keyboard.is_held(xenon::Key::W)) {
+        rect.pos.y -= PLAYER_SPEED * ctx.timer.dt();
+        look_point.center = Vector2{rect.pos.x + PLAYER_SIZE/2, rect.pos.y};
+    }
+    else if (ctx.keyboard.is_held(xenon::Key::S)) {
+        rect.pos.y += PLAYER_SPEED * ctx.timer.dt();
+        look_point.center = Vector2{rect.pos.x + PLAYER_SIZE/2, rect.pos.y + PLAYER_SIZE};
+    }
 
-    if (ctx.keyboard.is_held(xenon::Key::A)) {rect.pos.x -= PLAYER_SPEED * ctx.timer.dt();}
-    if (ctx.keyboard.is_held(xenon::Key::D)) {rect.pos.x += PLAYER_SPEED * ctx.timer.dt();}
+    // Horizontal movement
+    if (ctx.keyboard.is_held(xenon::Key::A)) {
+        rect.pos.x -= PLAYER_SPEED * ctx.timer.dt();
+        look_point.center = Vector2{rect.pos.x, rect.pos.y + PLAYER_SIZE/2};
+    }
+    else if (ctx.keyboard.is_held(xenon::Key::D)) {
+        rect.pos.x += PLAYER_SPEED * ctx.timer.dt();
+        look_point.center = Vector2{rect.pos.x + PLAYER_SIZE, rect.pos.y + PLAYER_SIZE/2};
+    }
+
+    // Attack
+    if (ctx.mouse.is_pressed(xenon::MouseButton::LeftButton)) {}
 }
 
 void Player::render(Context& ctx) {
     ctx.renderer.draw(drawable::make(rect, RED));
+    ctx.renderer.draw(drawable::make(look_point, GREEN));
 }
 
 void Player::finish() {
--- src/game/player.hpp
+++ src/game/player.hpp
@@ -2,10 +2,12 @@
 using namespace xenon;
 
 #define PLAYER_SPEED 100
+#define PLAYER_SIZE 20
 
 class Player {
     private:
         Rectangle rect;
+        Circle look_point;
     public:
         Player();
         ~Player();
